de484c2147cfc7a8e4342ad19be42fde2389b10c
kjojo0970-stack <Kjojo0970@gmail.com>
2026-07-18 19:23:43 +0300

    first commit

---
+++ app.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int price = 100; // Declare A New Variable
+    price = 200;     // Update The Value
+    cout << price;
+    return 0;
+}
