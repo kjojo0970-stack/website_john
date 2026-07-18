43cab287a9c4f104cc6dbcf8bd52606144ca7cf0
kjojo0970-stack <Kjojo0970@gmail.com>
2026-07-18 19:37:24 +0300

    update

--- app.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int price = 100; // Declare A New Variable
-    price = 200;     // Update The Value
-    cout << price;
-    return 0;
-}
